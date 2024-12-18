07df790824c0884fae1ac566686daa66fc476d2a
procub3r <sripradp@proton.me>
2024-12-18 16:19:00 +0530

    Implement ELF loading to load test binaries
    Gotta _love_ Zig for this. I mean what other language ships an ELF
    program header iterator in its stdlib?

--- src/main.zig
+++ src/main.zig
@@ -2,49 +2,78 @@ const std = @import("std");
 const Bus = @import("core.zig").Bus;
 const Core = @import("core.zig").Core;
 
-pub fn main() !void {
-    std.debug.print("Run `zig build test`\n", .{});
-}
+// Test a single test binary
+test "rv32ui-p-addi" {
+    // Open the ELF file and parse the ELF header
+    const elf_file = try std.fs.cwd().openFile("riscv-tests/isa/rv32ui-p-addi", .{});
+    const elf_hdr = try std.elf.Header.read(elf_file);
 
-test "rv32ui" {
-    // Memory to run tests
-    const TestMemory = struct {
-        const Self = @This();
-        raw: [0x10000]u8,
-        offset: usize, // Address of first byte in raw
-        bus: Bus,
+    // All test binaries use memory starting from their entrypoints
+    var memory = TestMemory.init(elf_hdr.entry);
 
-        pub fn init(offset: usize) Self {
-            return Self{
-                .raw = .{0} ** 0x10000,
-                .offset = offset,
-                .bus = .{
-                    .readByte = readByte,
-                    .writeByte = writeByte,
-                },
-            };
-        }
+    // Iterate through all the program headers
+    var program_headers = elf_hdr.program_header_iterator(elf_file);
+    while (try program_headers.next()) |phdr| {
+        // Ignore non-loadable segments
+        if (phdr.p_type != std.elf.PT_LOAD) continue;
 
-        fn readByte(bus_ptr: *Bus, addr_: u32) error{InaccessibleAddress}!u8 {
-            const self: *const Self = @fieldParentPtr("bus", bus_ptr);
-            const addr = addr_ - self.offset;
-            if (addr >= self.raw.len) return error.InaccessibleAddress;
-            return self.raw[addr];
-        }
+        // Load segment to test memory
+        try memory.loadSegment(phdr, elf_file);
+    }
 
-        fn writeByte(bus_ptr: *Bus, addr_: u32, byte: u8) error{InaccessibleAddress}!void {
-            const self: *Self = @fieldParentPtr("bus", bus_ptr);
-            const addr = addr_ - self.offset;
-            if (addr >= self.raw.len) return error.InaccessibleAddress;
-            self.raw[addr] = byte;
-        }
-    };
-
-    // Create the memory and the core
-    var memory = TestMemory.init(0);
+    // Create the RISC-V core
     var core = Core.init(&memory.bus);
+    // Set PC to the entry point of the test binary
+    core.pc = @bitCast(@as(u32, @intCast(elf_hdr.entry)));
 
     // Run an instruction!
-    // This must raise an illegal-instruction exception because the memory is un-initialized
     try core.step();
+    core.dump();
+}
+
+// Memory to run tests
+const TestMemory = struct {
+    const Self = @This();
+    raw: [0x10000]u8, // Memory region used by test processes
+    offset: usize, // Memory region start address
+    bus: Bus,
+
+    pub fn init(offset: usize) Self {
+        return Self{
+            .raw = .{0} ** 0x10000,
+            .offset = offset,
+            .bus = .{
+                .readByte = readByte,
+                .writeByte = writeByte,
+            },
+        };
+    }
+
+    /// Load segment from an ELF file to raw memory
+    pub fn loadSegment(self: *Self, phdr: std.elf.Elf64_Phdr, elf_file: std.fs.File) !void {
+        const start = phdr.p_vaddr - self.offset;
+        const end = start + phdr.p_filesz;
+        try elf_file.seekableStream().seekTo(phdr.p_offset);
+        try elf_file.reader().readNoEof(self.raw[start..end]);
+        // It is not necessary to zero extend loaded segments
+        // because all segments have p_memsz equal to p_filesz.
+    }
+
+    fn readByte(bus_ptr: *Bus, addr_: u32) error{InaccessibleAddress}!u8 {
+        const self: *const Self = @fieldParentPtr("bus", bus_ptr);
+        const addr = addr_ - self.offset;
+        if (addr >= self.raw.len) return error.InaccessibleAddress;
+        return self.raw[addr];
+    }
+
+    fn writeByte(bus_ptr: *Bus, addr_: u32, byte: u8) error{InaccessibleAddress}!void {
+        const self: *Self = @fieldParentPtr("bus", bus_ptr);
+        const addr = addr_ - self.offset;
+        if (addr >= self.raw.len) return error.InaccessibleAddress;
+        self.raw[addr] = byte;
+    }
+};
+
+pub fn main() !void {
+    std.debug.print("Run `zig build test`\n", .{});
 }
